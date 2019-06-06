4a932c864c92af31fb509c9d5b945cff98940d97
Dulio Denis <dulio.denis@yahoo.com>
2019-06-06 14:29:35 -0400

    Use setState() to update image when random int is generated

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -27,17 +27,19 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
-  int ballNumber = 0;
+  int ballNumber = 1;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
         onPressed: () {
-          ballNumber = Random().nextInt(5);
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
           print(ballNumber);
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball${ballNumber}.png'),
       ),
     );
   }
