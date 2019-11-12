aec91b5727419a802519fe384aa0da50862a8076
Dulio Denis <dulio.denis@yahoo.com>
2019-11-12 08:54:22 -0500

    Updated Image Literals in array

--- magic-8-ball-iOS13/Magic 8 Ball/ViewController.swift
+++ magic-8-ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball1")]
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
 
     @IBOutlet weak var imageView: UIImageView!
     
