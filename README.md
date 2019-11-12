4ab1d7286b9c53d931a8017158daad411426c3cf
Dulio Denis <dulio.denis@yahoo.com>
2019-11-11 20:34:17 -0500

    Used code to change the 8 ball image

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Magic 8-Ball
-An iOS and Android fortune telling app that animates a magic 8-ball to reveal the answer to an asked question. Written in Flutter 1.5 / Dart 2.3.
+An iOS and Android fortune telling app that animates a magic 8-ball to reveal the answer to an asked question. Written in Flutter 1.5 / Dart 2.3 with an Swift 5.1 / Xcode 11 version also included.
 
 ![](art/screenshot/magic-8-ball-04.gif?raw=true)
+![](art/screenshot/magic-8-ball-ios-00.gif?raw=true)
 
 ### Licensing
 Magic 8-Ball is licensed under [the MIT License](LICENSE).
--- magic-8-ball-iOS13/Magic 8 Ball/ViewController.swift
+++ magic-8-ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball1")]
 
-
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = ballArray[0]
+    }
+    
+    @IBAction func ask(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in: 0 ... 4)]
+    }
 }
 
